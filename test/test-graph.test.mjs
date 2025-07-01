7de31636034af7da3007a7130ebf28edc609992a
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-02 09:46:59 +1000

    add test for adding edge shorthand

--- test/test-graph.test.mjs
+++ test/test-graph.test.mjs
@@ -18,4 +18,13 @@ describe('The graph class', () => {
 
     expect(graph.get('A')).to.equal(vertex)
   })
+
+  it('Allows adding an edge directly without having to get the vertex', () => {
+    let graph = new Graph()
+    graph.add(new Vertex('A'), new Vertex('B'))
+    graph.addEdge('A', 'B', { weight: 5, attrs: { name: 'test' }})
+    expect(graph.get('A').getOutgoingEdges().length).to.equal(1)
+    expect(graph.get('A').getOutgoingEdges()[0].getDest()).to.equal(graph.get('B'))
+    expect(graph.get('A').getOutgoingEdges()[0].getAttr('name')).to.equal('test')
+  })
 })
