9acdf3c5c81feb5b41155aec75a07b75534164e6
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-01 20:47:59 +1000

    initial commit

---
+++ test/test-graph.test.mjs
@@ -0,0 +1,21 @@
+import { expect } from 'chai'
+import Graph from '../lib/Graph.mjs'
+import Vertex from '../lib/Vertex.mjs'
+
+describe('The graph class', () => {
+  it('Allows adding vertices', () => {
+    let graph = new Graph()
+    let vertex = new Vertex('A')
+    graph.add(vertex)
+
+    expect(graph.countVertices()).to.equal(1)
+  })
+
+  it('Allows getting a vertex by its name', () => {
+    let graph = new Graph()
+    let vertex = new Vertex('A')
+    graph.add(vertex)
+
+    expect(graph.getVertex('A')).to.equal(vertex)
+  })
+})
