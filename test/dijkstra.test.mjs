168cb7c3274daa0e042dcc532e90e629bcba689d
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-02 10:21:15 +1000

    add backtrack test

--- test/dijkstra.test.mjs
+++ test/dijkstra.test.mjs
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import Graph from '../lib/Graph.mjs'
 import Vertex from '../lib/Vertex.mjs'
-import { dijkstra } from '../lib/Dijkstra.mjs'
+import { backtrack, dijkstra } from '../lib/Dijkstra.mjs'
 
 describe('Dijkstra', () => {
   it('Finds the shortest path from a source to all vertices', () => {
@@ -64,5 +64,30 @@ describe('Dijkstra', () => {
       acc[i] = e
       return acc
     }, {}))
+  })
+})
+
+describe('Path reconstruction', () => {
+  it('Backtracks to find the vertices visited', () => {
+    let edges = [[6,0,3,1], [6,7,4,3], [6,5,6,2], [5,7,10,5], [4,8,8,5], [5,4,8,2],
+      [8,9,1,2], [7,8,1,3], [8,3,2,3], [1,10,5,4], [0,1,10,3], [10,2,7,2],
+      [3,2,15,2], [9,3,2,2], [2,4,10,5]]
+
+    let graph = new Graph()
+    let vertices = new Set()
+    let vertexIDs = edges.map(edge => edge.slice(0, 2)).reduce((a, e) => a.concat(e), []).filter(id => {
+      if (vertices.has(id)) return false
+      return vertices.add(id) || true
+    })
+    for (let vertex of vertexIDs) graph.add(new Vertex(vertex))
+    for (let edge of edges) {
+      graph.addEdge(edge[0], edge[1], { weight: edge[2] })
+    }
+
+    let { dist, pred } = dijkstra(graph, 6)
+    let path = backtrack(pred, 3)
+
+    let expectedSteps = [6, 7, 8, 3].map(vertex => graph.get(vertex))
+    expect(path).to.equal(expectedSteps)
   })
 })
