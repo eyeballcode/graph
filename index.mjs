14aef0f2dae7c9257392105211339b23be2eb655
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-02 10:14:51 +1000

    add index.mjs to expose classes

---
+++ index.mjs
@@ -0,0 +1,17 @@
+import Heap from './lib/heap/Heap.mjs';
+import MinHeap from './lib/heap/MinHeap.mjs';
+import MaxHeap from './lib/heap/MaxHeap.mjs';
+import { dijkstra } from './lib/Dijkstra.mjs';
+import Edge from './lib/Edge.mjs';
+import Vertex from './lib/Vertex.mjs';
+import Graph from './lib/Graph.mjs';
+
+export default {
+  Heap,
+  MinHeap,
+  MaxHeap,
+  dijkstra,
+  Graph,
+  Vertex,
+  Edge
+}
